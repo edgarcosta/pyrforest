8f43e0888911178f03b6115531cce74ee10f4f57
Edgar Costa <edgarc@mit.edu>
2023-09-25 22:46:58 -0400

    a,b -> a/b = gamma

--- pyrforest/batch.py
+++ pyrforest/batch.py
@@ -4,7 +4,7 @@ from sage.matrix.constructor import Matrix
 from sage.rings.fast_arith import prime_range
 from sage.rings.integer_ring import ZZ
 
-def batch_factorial(n, e, a, b):
+def batch_factorial(n, e, gamma):
     r"""
     Amortized computation of factorials.
 
@@ -12,13 +12,15 @@ def batch_factorial(n, e, a, b):
 
      - `n`: bound on primes
      - `e`: order of the series expansion
-     - `a/b`: a rational number
+     - `gamma`: a rational number
 
     OUTPUT::
-       A dict whose value at a prime `p` equals (ceil(a/b*p)-1)! mod p^e.
+       A dict whose value at a prime `p` equals (ceil(gamma*p)-1)! mod p^e.
     """
     R = ZZ["k"]
     M = Matrix(R, 1, 1, [R.gen()])
+    a = gamma.numerator()
+    b = gamma.denominator()
     k = lambda p, a=a, b=b: -(-a * p // b)
     m = lambda p, e=e: p**e
 
@@ -26,7 +28,7 @@ def batch_factorial(n, e, a, b):
     return {p: ans[p][0, 0] for p in prime_range(n)}
 
 
-def batch_harmonic(n, e, a, b, j, proj=False):
+def batch_harmonic(n, e, gamma, j, proj=False):
     r"""
     Amortized computation of harmonic sums.
 
@@ -34,12 +36,12 @@ def batch_harmonic(n, e, a, b, j, proj=False):
 
      - `n`: bound on primes
      - `e`: order of the series expansion
-     - `a/b`: a rational number
+     - `gamma`: a rational number
      - `j`: a positive integer
 
     OUTPUT::
        A dict whose value at a prime `p` is the truncated harmonic sum
-          \sum_{k=1}^{ceil(a/b*p)-1} k^{-j} mod p^e.
+          \sum_{k=1}^{ceil(gamma*p)-1} k^{-j} mod p^e.
 
        If `proj` is True, instead return pairs (x,y) representing x,y mod p^e.
     """
@@ -48,6 +50,8 @@ def batch_harmonic(n, e, a, b, j, proj=False):
     y = R.gen()
     M = Matrix(R, [[y**j, 0], [1, y**j]])
 
+    a = gamma.numerator()
+    b = gamma.denominator()
     k = lambda p, a=a, b=b: -(-a * p // b)
     m = lambda p, e=e: p**e
     V = Matrix(ZZ, [[0, 1]])
