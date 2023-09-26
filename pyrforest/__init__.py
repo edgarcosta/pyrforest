1846d4f6b6e56c9eb1fc2e5e62a26f268ecc7c89
Edgar Costa <edgarc@mit.edu>
2023-09-25 22:23:06 -0400

    __init__

---
+++ pyrforest/__init__.py
@@ -0,0 +1,13 @@
+from .rforest import (
+    remainder_forest,
+    remainder_forest_generic_prime,
+)
+assert remainder_forest
+assert remainder_forest_generic_prime
+
+from .batch import (
+    batch_factorial,
+    batch_harmonic,
+)
+assert batch_factorial
+assert batch_harmonic 
